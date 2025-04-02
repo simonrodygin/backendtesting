1e9ce4e3d3e0794964da9606db36d351328844e9
simon <simonrodygin@gmail.com>
2025-04-02 17:15:14 +0300

    в хелпер можно засунуть что угодно

--- services/university/helpers/grade_helper.py
+++ services/university/helpers/grade_helper.py
@@ -11,11 +11,13 @@ class GradeHelper(BaseHelper):
         response = self.session_utils.get(self.ENDPOINT)
         return response
     
-    def get_stats(self, search_by_id: Dict[Literal['group_id', 'student_id', 'teacher_id'], Any] | None = None):
+    def get_stats(self, search_by_id = None):
         if search_by_id is None:
             response = self.session_utils.get(self.STATS_ENDPOINT)
-        else: 
+        elif isinstance(search_by_id, Dict[Literal['group_id', 'student_id', 'teacher_id'], Any]): 
             search_by_id = {key: value for key, value in search_by_id.items() if value is not None}
+            response = self.session_utils.get(self.STATS_ENDPOINT, params=search_by_id)
+        else:
             response = self.session_utils.get(self.STATS_ENDPOINT, params=search_by_id)
         return response
 
